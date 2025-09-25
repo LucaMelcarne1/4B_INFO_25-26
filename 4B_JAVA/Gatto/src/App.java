3a9b0b9c3e4a67802ff518ea4954177ba4060703
LucaMelcarne1 <luca.melcarne@salveminialessano.edu.it>
2025-09-25 11:26:35 +0200

    Delete 4B_JAVA/Gatto directory

--- 4B_JAVA/Gatto/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Gatto gatto1 = new Gatto();
-        gatto1.miagola();
-        Gatto.ciao();
-        
-    }
-}
