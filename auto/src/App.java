46e5eb869fdab886d43fdd7a5a570e4afe0fd982
LucaMelcarne1 <luca.melcarne@salveminialessano.edu.it>
2025-11-04 13:55:50 +0100

    Auto
    Aggiunta classe Pilota

--- auto/src/App.java
+++ auto/src/App.java
@@ -1,10 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //istanza classe Auto
         Auto auto1= new Auto("Fiat","Panda","rossa",5);  
+        //istanza classe Pilota
+        Pilota pilota1=new Pilota("Rossi","Mario",1990,true);    
         auto1.accendi();
         auto1.percorri(50);
         auto1.percorri(510);
         auto1.spegni();
         System.out.println("km percorsi: "+ auto1.kmPercorsi);
+
+
     }
 }
---
+++ auto/src/Pilota.java
@@ -0,0 +1,15 @@
+public class Pilota {
+    String nome;
+    String cognome;
+    int anno_nascita;
+    boolean patente;
+
+    public Pilota(String nome, String cognome, int anno_nascita, boolean patente) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.anno_nascita = anno_nascita;
+        this.patente = patente;
+    }
+    public Pilota() {
+    }
+}
